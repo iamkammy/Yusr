7c2aae23f66abf16a0025cfc8c3efb276b730c5e
Kamran-Khan <kamran.khan_ext@moxey.ai>
2022-08-15 07:15:02 +0530

    Frontend Base Setup

---
+++ frontend/build-utils/webpack.common.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: path.resolve(__dirname, '..', './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader', 'eslint-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx'],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Advanced React with Webpack Setup',
+      template: path.resolve(__dirname, '..', './src/index.html'),
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, '..', 'dist'),
+    filename: 'bundle.js',
+  },
+};
